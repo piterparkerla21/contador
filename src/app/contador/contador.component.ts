4174f5acbd4a7d4a636240f929043e284320e2db
yon_ruiz <yon_ruiz@soy.sena.edu.co>
2024-11-18 15:04:53 -0500

    cambio 1 11-18-2024 15-04

---
+++ src/app/contador/contador.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+@Component({
+    selector:'app-contador',
+    templateUrl:'./contador.component.html',
+    styleUrl:'./contador.component.css'
+})
+export class ContadorComponent{
+
+}
